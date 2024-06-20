31e74c7033ab3d24074f5564becdad19622a64c3
haruto-takeuchi <haruto.takeuchi@esm.co.jp>
2024-06-20 08:31:09 +0000

    main.go作成（Springf()関数でエラーあり）

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/hello/:name", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		ctx.String(http.StatusOK, makeGreeting(name))
+		router.Run(":8080")
+	})
+}
+
+func makeGreeting(name string) string {
+	return fmt.Sprintf("Hello, %d", name)
+}
